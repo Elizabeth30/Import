8ee35ab161da46c9ba19369728df145872b0dfb6
Elizabeth <ElizabethSanchez@crbc.net>
2015-04-23 09:12:37 -0400

    Initial Commit

---
+++ Import/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Import
+//
+//  Created by Liz Sanchez on 4/23/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
